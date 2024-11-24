a10847bf4199eb40e0b2c8e5b0aadf878a676ba1
praveen-kumar-ketavat <157162919+praveen-kumar-ketavat@users.noreply.github.com>
2024-11-24 11:33:06 +0530

    Update App.js

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
   });
 
   async function getData() {
-    const response = await axios.get("http://localhost:3000/students");
+    const response = await axios.get("https://nodeproject-backend-thox.onrender.com");
     setData(response.data);
   }
 
